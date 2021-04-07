9f93ccc68137a1a56c0658ea3fe65ba7574c66c6
burczan <natalia.wajnor@gmail.com>
2021-04-07 20:47:15 +0200

    setup project

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>Under Pressure</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Under Pressure</h1>
+    </>
+  );
+};
+
+export default Home;
